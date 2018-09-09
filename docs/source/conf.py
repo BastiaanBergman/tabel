24d7628004291540e79af233237cf61bc4e126a4
bastiaanbergman <bastiaan@atomwise.com>
2018-09-09 00:15:36 -0700

    Added recfunctions and itertools as mock module

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath('../../'))
 
 import mock
 
-MOCK_MODULES = ['numpy']
+MOCK_MODULES = ['numpy', 'numpy.lib.recfunctions', 'itertools']
 for mod_name in MOCK_MODULES:
    sys.modules[mod_name] = mock.Mock()
 
