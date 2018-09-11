3870e1c138057429bf9a1e6bdba134c1b7fc1148
bastiaanbergman <bastiaan@atomwise.com>
2018-09-11 09:51:10 -0700

    fix PyPi import error

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.rst", "r") as fh:
 setuptools.setup(
     name="tabel",
     keywords='data table',
-    version="1.0.2",
+    version="1.0.3",
     author="Bastiaan Bergman",
     author_email="Bastiaan.Bergman@gmail.com",
     description="Lightweight, intuitive and fast data-tables.",
--- tabel/__init__.py
+++ tabel/__init__.py
@@ -1,3 +1,3 @@
 name = "tabel"
-from tabel import Tabel, read_tabel, first, transpose, T
+from .tabel import Tabel, read_tabel, first, transpose, T
 __all__ = ["Tabel", "first", "transpose", "T", "read_tabel"]
